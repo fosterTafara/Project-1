e113be9a0469fa784b0d9ff517be9b64b26daff2
freedomlife <30481241+freedomlife@users.noreply.github.com>
2019-05-08 11:53:54 +1200

    Update devapp.py

--- devapp.py
+++ devapp.py
@@ -96,13 +96,13 @@ def deviceborrowreturn(userid):
 	if request.method == 'POST':
 
 		if 'ReturnNow' in request.form:
-			mycursor = mydb.cursor()
-			mycursor.execute("select device.deviceId,device.deviceName, device.deviceType from device inner join checkingsystem on device.deviceId = checkingsystem.deviceId where checkingsystem.userId = %s AND checkingsystem.returnDate is NULL", (user_id,))		
-			loan_devices = mycursor.fetchall()
-			num_device = len(loan_devices)
+			# mycursor = mydb.cursor()
+			# mycursor.execute("select device.deviceId,device.deviceName, device.deviceType from device inner join checkingsystem on device.deviceId = checkingsystem.deviceId where checkingsystem.userId = %s AND checkingsystem.returnDate is NULL", (user_id,))		
+			# loan_devices = mycursor.fetchall()
+			# num_device = len(loan_devices)
 			print('number of devices')
 			print(num_device)
-			mycursor.close()
+			# mycursor.close()
 			mycursor = mydb.cursor()
 			SelectedDevices = request.form.getlist('selected[]')
 			print(SelectedDevices)
